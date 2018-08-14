757f61551035ad96aac89208d92ae65743c90511
AlidaContreras <alidacontreras01@gmail.com>
2018-08-14 00:06:33 -0500

    Cuando se desea eliminar el post, ya pide confirmación

--- src/data2.js
+++ src/data2.js
@@ -1,11 +1,11 @@
 const db = firebase.firestore();
-const speech =() =>{// esta funcion es el codigo que sale en el error grandote
+const speech = () => {// esta funcion es el codigo que sale en el error grandote
 	firestore = firebase.firestore();
 	const settings = { timestampsInSnapshots: true };
 	firestore.settings(settings);
 }
 const savePost = () => {//esta funcion es la que guarda el ID, EMAIL Y STRING(post) en fireStore
-	speech ();
+	speech();
 	db.collection("POST`s").add({
 		id: idUserInLine,
 		email: emailUserInLine,
@@ -22,33 +22,61 @@ const savePost = () => {//esta funcion es la que guarda el ID, EMAIL Y STRING(po
 }
 
 //leer documentos
-const readPost = () =>{
+const readPost = () => {
 	speech();
 	db.collection("POST`s").onSnapshot((querySnapshot) => {
 		boxPosteado.innerHTML = "";
 		querySnapshot.forEach((doc) => {
 			console.log(`${doc.id} => ${doc.data().post}`);
-			boxPosteado.innerHTML += `
+			boxPosteado.innerHTML +=
+				`
 			<br>
 			<br><div class="z-depth-3 input-field col s10">
         <p>${doc.data().post}</p>
       </div>
       <br>
-      <button type="submit" id="buttonAdd" class="btn orange">Editar</button>
+      <button type="submit" id="buttonAdd" class="btn orange" onclick = "editPost('${doc.id}','${doc.data().post}')">Editar</button>
       <!-- Modal Trigger -->
-      <a class="waves-effect btn yellow darken-2" onclick = deletePost('${doc.id}')>Eliminar</a>
+      <button class="waves-effect btn yellow darken-2" onclick = "deletePost('${doc.id}')">Eliminar</button>
 			`
 		});
 	});
 }
 readPost();
 
-const deletePost = (a) => {
-	db.collection("POST`s").doc(a).delete().then(function () {
-		console.log("Document successfully deleted!");
-	}).catch(function (error) {
-		console.error("Error removing document: ", error);
-	});
+const deletePost = (id) => {
+	const result = confirm("¿Estas segurx que deseas eliminar el post?");
+	if (result == true) {
+		db.collection("POST`s").doc(id).delete().then(function () {
+			console.log("Document successfully deleted!");
+		}).catch(function (error) {
+			console.error("Error removing document: ", error);
+		});
+	}
+}
+
+const editPost = (id, post) => {
+
+	document.getElementById('textarea2').value = post;
+
+	const button = document.getElementById('publicarPost');
+	button.innerHTML = 'Editar';
+
+	button.onclick = () => {
+		var washingtonRef = db.collection("POST`s").doc(id);
+
+		return washingtonRef.update({
+			post: post,
+		})
+			.then(function () {
+				console.log("Document successfully updated!");
+				button.innerHTML = 'Guardar';
+			})
+			.catch(function (error) {
+				// The document probably doesn't exist.
+				console.error("Error updating document: ", error);
+			});
+	}
 }
 
 logout = () => {

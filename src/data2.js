7742d253bc462159b7c4885d546f3c6eb2af8c5c
AlidaContreras <alidacontreras01@gmail.com>
2018-08-16 15:23:56 -0500

    Ya se puede ver los post publicos y privados aparte

--- src/data2.js
+++ src/data2.js
@@ -6,6 +6,7 @@ const speech = () => {// esta funcion es el codigo que sale en el error grandote
 }
 const savePost = () => {//esta funcion es la que guarda el ID, EMAIL Y STRING(post) en fireStore
 	speech();
+	//guardar los post privados en una coleccion PostsPrivados
 	if (privacity.value == "SoloYo") {
 		db.collection("PostsPrivados").add({
 			id: idUserInLine,
@@ -23,7 +24,8 @@ const savePost = () => {//esta funcion es la que guarda el ID, EMAIL Y STRING(po
 		event.preventDefault();
 
 	}
-	else if (privacity.value == "Todos")
+	//guardar los post publicosen una coleccion PostsPublicos
+	else if (privacity.value == "Todos") {
 		db.collection("PostsPublicos").add({
 			id: idUserInLine,
 			email: emailUserInLine,
@@ -37,19 +39,17 @@ const savePost = () => {//esta funcion es la que guarda el ID, EMAIL Y STRING(po
 			.catch((error) => {
 				console.error("Error adding document: ", error);
 			});
-	event.preventDefault();
+		event.preventDefault();
+	}
 }
-//leer documentos
-
-const readPost = () => {
+//Esta función lee los posts PUBLICOS y los muestra en pantalla
+const readPublicPost = () => {
 	speech();
 	db.collection("PostsPublicos").onSnapshot((querySnapshot) => {
 		boxPosteado.innerHTML = "";
 		querySnapshot.forEach((doc) => {
-			console.log(idUserInLine);
-			console.log(`${doc.id} => ${doc.data().post} => ${doc.data().like} =>${doc.data().id}`);
 
-			if (idUserInLine != `${doc.data().id}` ) {
+			if (idUserInLine != `${doc.data().id}`) {
 				boxPosteado.innerHTML +=
 					`
 					<br>
@@ -58,7 +58,7 @@ const readPost = () => {
 					<textarea class="materialize-textarea textarea-custom-padding" disabled id="elPost-${doc.id}" cols="30" rows="10">${doc.data().post}</textarea>
 					</div>
 					<br>
-					<button class="btn blue rigth buttons" id = "like-${doc.id}" onclick = "likePost('${doc.id}','${doc.data().like}')" >${doc.data().like} Me gusta</button>
+					<button class="btn blue rigth buttons" id = "like-${doc.id}" onclick = "likePublicPost('${doc.id}','${doc.data().like}')", "likePrivatePost('${doc.id}','${doc.data().like}')"  >${doc.data().like} Me gusta</button>
 					`
 			}
 
@@ -77,31 +77,68 @@ const readPost = () => {
 					<button class="btn orange buttons" id="buttonAdd-${doc.id}"  onclick="editPost('${doc.id}', '${doc.data().post}')" >Editar</button>
 					`
 			}
-					});
+		});
 		/* 	const buttonLikes= getElementById(${doc.data().id}) */
 	});
 }
-readPost();
-
-const deletePost = (id) => {
+readPublicPost();
+//Esta función lee los posts PRIVADOS y los muestra en pantalla
+const readPrivatePost = () => {
+	speech();
+	db.collection("PostsPrivados").onSnapshot((querySnapshot) => {
+		boxPosteado.innerHTML = "";
+		const selectViewPost = document.getElementById('selectViewPost');
+		querySnapshot.forEach((doc) => {
+			if (idUserInLine == `${doc.data().id}`) {
+				console.log(`${doc.id} => ${doc.data().post} => ${doc.data().like}`);
+				boxPosteado.innerHTML +=
+					`
+					<br>
+					<br><div class="z-depth-3 input-field col s10">
+					<p> ${doc.data().email} </p>
+					<textarea class="materialize-textarea textarea-custom-padding" disabled id="elPost-${doc.id}" cols="30" rows="10">${doc.data().post}</textarea>
+					</div>
+					<br>
+					<button class="waves-effect btn red darken-2 buttons" onclick = "deletePost('${doc.id}')">Eliminar</button>
+					<button class="btn blue rigth buttons" id = "like-${doc.id}" onclick = "likePost('${doc.id}','${doc.data().like}')" >${doc.data().like} Me gusta</button>
+					<button class="btn orange buttons" id="buttonAdd-${doc.id}"  onclick="editPost('${doc.id}', '${doc.data().post}')" >Editar</button>
+					`
+			}
+		});
+		/* 	const buttonLikes= getElementById(${doc.data().id}) */
+	});
+}
+//esta funcion es para eliminar los pos Publicos
+const deletePublicPost = (id) => {
 	const result = confirm("¿Estas segurx que deseas eliminar el post?");
 	if (result == true) {
-		db.collection("POST`s").doc(id).delete().then(() => {
+		db.collection("PostsPublicos").doc(id).delete().then(() => {
 			console.log("Document successfully deleted!");
 		}).catch((error) => {
 			console.error("Error removing document: ", error);
 		});
 	}
 }
-
-const editPost = (id, post) => {
+//Esta funcion es para eliminar los pos Publicos
+const deletePrivatePost = (id) => {
+	const result = confirm("¿Estas segurx que deseas eliminar el post?");
+	if (result == true) {
+		db.collection("PostsPrivados").doc(id).delete().then(() => {
+			console.log("Document successfully deleted!");
+		}).catch((error) => {
+			console.error("Error removing document: ", error);
+		});
+	}
+}
+//editar post publicos
+const editPublicPost = (id, post) => {
 	const cuadroPost = document.getElementById(`elPost-${id}`);
 	cuadroPost.disabled = false;
 	document.getElementById(`elPost-${id}`).value = post;
 	const button = document.getElementById(`buttonAdd-${id}`);
 	button.innerHTML = 'Guardar';
 	button.onclick = () => {
-		const washingtonRef = db.collection("POST`s").doc(id);
+		const washingtonRef = db.collection("PostsPublicos").doc(id);
 		const post = document.getElementById(`elPost-${id}`).value;
 		return washingtonRef.update({
 			post
@@ -117,16 +154,59 @@ const editPost = (id, post) => {
 			});
 	}
 }
-const likePost = (id, cantActual) => {
+//ediatr posts privados
+const editPrivatePost = (id, post) => {
+	const cuadroPost = document.getElementById(`elPost-${id}`);
+	cuadroPost.disabled = false;
+	document.getElementById(`elPost-${id}`).value = post;
+	const button = document.getElementById(`buttonAdd-${id}`);
+	button.innerHTML = 'Guardar';
+	button.onclick = () => {
+		const washingtonRef = db.collection("PostsPrivados").doc(id);
+		const post = document.getElementById(`elPost-${id}`).value;
+		return washingtonRef.update({
+			post
+		})
+			.then(() => {
+				// console.log("Document successfully updated!");
+				button.innerHTML = 'Editar';
+				button.onclick = editPost;
+			})
+			.catch((error) => {
+				// The document probably doesn't exist.
+				console.error("Error updating document: ", error);
+			});
+	}
+}
+//like a los post publicos
+const likePublicPost = (id, cantActual) => {
 	cantActual++;
-	const washingtonRef = db.collection("POST`s").doc(id);
+	const washingtonRef = db.collection("PostsPublicos").doc(id);
 	document.getElementById(`like-${id}`).value = cantActual;
 	return washingtonRef.update({
 		like: cantActual
 	})
 		.then(() => {
 			console.log("Document successfully updated!");
-			readPost()
+			readPostPublicos();
+		})
+		.catch((error) => {
+			// The document probably doesn't exist.
+			console.error("Error updating document: ", error);
+		});
+	event.preventDefault();
+}
+//like a los post privados
+const likePrivatePost = (id, cantActual) => {
+	cantActual++;
+	const washingtonRef = db.collection("PostsPrivados").doc(id);
+	document.getElementById(`like-${id}`).value = cantActual;
+	return washingtonRef.update({
+		like: cantActual
+	})
+		.then(() => {
+			console.log("Document successfully updated!");
+			readPostPublicos();
 		})
 		.catch((error) => {
 			// The document probably doesn't exist.
